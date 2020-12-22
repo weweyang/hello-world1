b772b717bd368a0c6a49a3094f64a74636414438
weweyang <xiang@163.com>
2020-12-22 11:44:31 +0800

    first commit

---
+++ hh.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class a{
+	a(){
+		System.out.println("aaaa");
+	}
+}
+
+class b extends a{
+	b(){
+		System.out.println("bbbb");
+	}
+}
+
+public class hh extends b{
+//	hh{
+//		System.out.println("ccccc");
+//	}
+	public static void main(String []args){
+		  hh c = new hh();
+	  }
+}
+
+
